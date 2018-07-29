d9248681e5cb509ace3ff3c5e8f2d0bdbccbf4a1
Taran Pierce <taran.pierce@endurance.com>
2018-07-29 10:59:26 -0600

    feature/logic-updates - working on the lightbox for the carousel

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -11,6 +11,8 @@ let carousel = new Siema({
   rtl: false,
   onInit: () => {
     carouselNavigation();
+    carouselView();
+    lightBox();
   },
   onChange: () => {},
 });
@@ -31,4 +33,76 @@ function carouselNavigation() {
       }
     });
   }
+}
+
+function carouselView() {
+  let toggle = document.getElementsByClassName('view-toggle')[0];
+  let container = document.getElementsByClassName('carousel')[0];
+  
+  toggle.addEventListener('click', function( e ) {
+    e.preventDefault();
+    container.classList.toggle('thumb-view');
+  });
+}
+
+function lightBox() {
+  let targets = document.getElementsByClassName('thumb');
+  
+  for ( let target of targets ) {
+    
+    target.addEventListener('click', function() {
+      let el = target.children[0];
+      
+      console.log('el ', el);
+      let href = el.getAttribute('href');
+  
+      let id = href.split('-')[2];
+  
+      console.log('id ' + id);
+      
+      createLightBox( id );
+      createBackDrop();
+    });
+  }
+}
+
+function createLightBox( id ) {
+  console.log('oh hai createLightBox ' + id);
+  
+  let contentContainer = document.createElement('div');
+  contentContainer.classList.add('light-box');
+  
+  let content = document.createElement('div');
+  content.classList.add('content');
+  contentContainer.appendChild( content );
+  
+  let lightBoxNav = document.createElement('div');
+  lightBoxNav.classList.add('light-box-nav');
+  let navToggle = document.createElement('a');
+  navToggle.innerHTML = "X";
+  // TODO finish styling this so that you can close it on mobile easily
+  lightBoxNav.appendChild( navToggle );
+  
+  contentContainer.appendChild( lightBoxNav );
+  
+  document.body.appendChild( contentContainer );
+}
+
+function createBackDrop() {
+  let backDrop = document.createElement('div');
+  
+  backDrop.classList.add('light-box-backdrop');
+
+  document.body.appendChild( backDrop );
+  
+  closeBackDrop( backDrop );
+}
+
+function closeBackDrop( backDrop ) {
+  backDrop.addEventListener('click', function() {
+    this.remove();
+  
+    let lightBox = document.getElementsByClassName('light-box')[0];
+    lightBox.remove();
+  });
 }
--- app/templates/_carousel.php
+++ app/templates/_carousel.php
@@ -11,20 +11,34 @@
 ?>
 
 <div class="carousel">
-  <div class="siema">
+  <div class="carousel-wrapper">
+    <div class="siema">
+      <?php
+        for ($i = 0; $i <= $count; $i++) {
+          print_r('<picture>');
+            print_r('<source srcset="' . $path . '1170x150" media="(min-width: 1200px)">');
+            print_r('<source srcset="' . $path . '9700x150" media="(min-width: 992px)">');
+            print_r('<source srcset="' . $path . '740x150" media="(min-width: 768px)">');
+            print_r('<img class="img-responsive" src="' . $path .'450x150">');
+          print_r('</picture>');
+        }
+      ?>
+    </div>
+  </div>
+  <div class="thumbnails">
     <?php
-      for ($i = 0; $i <= $count; $i++) {
-        print_r('<picture>');
-          print_r('<source srcset="' . $path . '1170x150" media="(min-width: 1200px)">');
-          print_r('<source srcset="' . $path . '9700x150" media="(min-width: 992px)">');
-          print_r('<source srcset="' . $path . '740x150" media="(min-width: 768px)">');
-          print_r('<img class="img-responsive" src="' . $path .'450x150">');
-        print_r('</picture>');
-      }
-    ?>
+        for ($i = 1; $i <= $count; $i++) {
+          print_r('<div class="thumb">');
+            print_r('<a href="#light-box-' . $i . '">');
+              print_r('<img class="img-responsive" src="' . $path .'150x150">');
+            print_r('</a>');
+          print_r('</div>');
+        }
+      ?>
   </div>
   <div class="controls">
     <a href="#prev">prev</a>
     <a href="#next">next</a>
+    <a href="#toggle" class="view-toggle">Toggle</a>
   </div>
 </div>
